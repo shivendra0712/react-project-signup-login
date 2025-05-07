4af7add671b2083b90c0b454d00c00b53ff69002
shivendra0712 <patelshivendra817@gmail.com>
2025-05-07 22:36:22 +0530

    connected signin and signup jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,23 @@
 import React, { useState } from 'react'
-import { useForm } from 'react-hook-form' 
+import { useForm } from 'react-hook-form'
+import Signup from './components/Signup'
+import Signin from './components/Signin'
+import Data from './components/Data'
 
 const App = () => {
 
   const [toggler, settoggler] = useState(false)
 
   return (
-    <div className='w-screen h-screen overflow-x-hidden'>
-      {toggler ? (<div className='w-screen h-screen bg-gray-800 text-white p-4 flex justify-center items-center'>
-        <form action="" className='w-full md:w-[30%] mt-10  bg-black/40    p-10 rounded-2xl flex flex-col gap-4 items-center'>
-          {/* {name , emial , password} */}
-          <h1 className='text-2xl'>Signup Form</h1>
-          <input type="text" placeholder='John Sharma' className='border black p-2 w-full outline-none  rounded' />
-          <input type="email" placeholder='Johnsharma@123gmail.com' className='border black p-2 w-full outline-none rounded' />
-          <input type="password" placeholder='John@123' className='border black p-2 w-full outline-none rounded' />
-          <button className='bg-gray-300 text-black w-[40%] py-2 rounded-lg font-medium hover:bg-gray-400 cursor-pointer hover:scale-95 '>Signup</button>
-          <small calassName='text-center'>
-            Already have an account?{'  '}
-            <button onClick={()=> settoggler(!toggler)} type='button'> Signin</button>
-          </small>
-        </form>
-      </div>) 
-      : (<div className='w-screen h-screen bg-gray-800 text-white p-4 flex justify-center items-center'>
-        <form action="" className='w-full md:w-[30%] mt-10  bg-black/40    p-10 rounded-2xl flex flex-col gap-4 items-center'>
-          {/* {name , emial , password} */}
-          <h1 className='text-2xl'>Login Form</h1>
-          <input type="email" placeholder='Johnsharma@123gmail.com' className='border black p-2 w-full outline-none rounded' />
-          <input type="password" placeholder='John@123' className='border black p-2 w-full outline-none rounded' />
-          <button className='bg-gray-300 text-black w-[40%] py-2 rounded-lg font-medium hover:bg-gray-400 cursor-pointer hover:scale-95'>Login</button>
-          <small calassName='text-center'>
-            Dodn't have an account? {'  '}
-            <button onClick={()=> settoggler(!toggler)} type='button'>Signup</button>
-          </small>
-        </form>
+    <div className='w-screen h-screen overflow-x-hidden flex bg-gray-800'>
+      <div className="w-[70%] h-screen">
+        {toggler ? <Signin toggler={toggler} settoggler={settoggler} />
+          : <Signup toggler={toggler} settoggler={settoggler} />}
       </div>
-      )}
-
-
+      <div className="w-[30%] ">
+ <Data/>
+      </div>
     </div>
-
-
   )
 }
 
