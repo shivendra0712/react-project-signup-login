419ac844e86783dadbbae97c7f094482c300b6e7
shivendra0712 <patelshivendra817@gmail.com>
2025-05-07 22:36:36 +0530

    added Data.jsx file

---
+++ src/components/Data.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Data = () => {
+    return (
+        <div className="w-full h-full p-6 text-white/90">
+
+
+            <div className='p-4  flex  justify-between   items-center bg-gray-700 rounded-lg mb-2'>
+                <ul className=' '>
+                    <li className='text-2xl font-base'>username</li>
+                    <li>user@123gmail.com</li>
+                </ul>
+                <button className='text-red-700 font-medium bg-white/40 px-2 py-1 rounded-full hover:cursor-pointer hover:bg-white/30'>Delete</button>
+            </div>
+            <div className='p-4  flex  justify-between   items-center bg-gray-700 rounded-lg  mb-2'>
+                <ul className=' '>
+                    <li className='text-2xl font-base'>username</li>
+                    <li>user@123gmail.com</li>
+                </ul>
+                <button className='text-red-700 font-medium bg-white/40 px-2 py-1 rounded-full hover:cursor-pointer hover:bg-white/30'>Delete</button>
+            </div>
+            <div className='p-4  flex  justify-between   items-center bg-gray-700 rounded-lg  mb-2'>
+                <ul className=' '>
+                    <li className='text-2xl font-base'>username</li>
+                    <li>user@123gmail.com</li>
+                </ul>
+                <button className='text-red-700 font-medium bg-white/40 px-2 py-1 rounded-full hover:cursor-pointer hover:bg-white/30'>Delete</button>
+            </div>
+        </div>
+    )
+}
+
+export default Data
